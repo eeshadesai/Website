10bd3ff2278aaf9dbf12ddeed026fc687db3c26a
Eesha Desai <ecdesai3@gmail.com>
2023-02-12 17:53:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,122 @@
+//set up the server
+const express = require( "express" );
+const logger = require("morgan");
+const app = express();
+const port = 8080;
+const db = require("./db/db_connection");
+
+// Configure Express to use EJS
+app.set( "views",  __dirname + "/views");
+app.set( "view engine", "ejs" );
+
+// define middleware that logs all incoming requests
+app.use(logger("dev"));
+
+// define middleware that serves static resources in the public directory
+app.use(express.static(__dirname + '/public'));
+// Configure Express to parse URL-encoded POST request bodies (traditional forms)
+app.use( express.urlencoded({ extended: false }) );
+
+// define a route for the default home page
+app.get( "/", ( req, res ) => {
+    res.render("index");
+} );
+
+// define a route for the stuff inventory page
+const read_stuff_all_sql = `
+    SELECT 
+        id, class, assignment, date
+    FROM
+    stuff
+`
+app.get( "/assignments", ( req, res ) => {
+    db.execute(read_stuff_all_sql, (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.render('assignments',{inventory : results});
+        }
+    });
+});
+
+// define a route for the item detail page
+const read_item_sql = `
+    SELECT 
+        class, assignment, date, description 
+    FROM
+    stuff
+    WHERE
+        id = ?
+`
+app.get( "/assignment/details/:id", ( req, res, ) => {
+    db.execute(read_item_sql, [req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); 
+        else if(results.length==0)
+            res.status(404).send(`No item found with id = "${req.params.id}"`);
+        else {
+            let data = results[0];
+            res.render('assignment_details', data);
+        }
+            
+    });
+});
+// define a route for item DELETE
+const delete_item_sql = `
+    DELETE 
+    FROM
+        stuff
+    WHERE
+        id = ?
+`
+app.get("/assignments/assignment_details/:id/delete", ( req, res ) => {
+    db.execute(delete_item_sql, [req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect("/assignments");
+        }
+    });
+})
+// define a route for item Create
+const create_item_sql = `
+    INSERT INTO stuff
+        (class, assignment, date)
+    VALUES
+        (?, ?, ?)
+`
+app.post("/assignments", ( req, res ) => {
+    db.execute(create_item_sql, [req.body.name, req.body.quantity], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            //results.insertId has the primary key (id) of the newly inserted element.
+            res.redirect(`/assignments/assignment_details/${results.insertId}`);
+        }
+    });
+})
+// define a route for item UPDATE
+const update_item_sql = `
+    UPDATE
+        stuff
+    SET
+        class = ?,
+        assignment = ?,
+        date = ?,
+        description = ?
+    WHERE
+        id = ?
+`
+app.post("/assignments/assignment_details/:id", ( req, res ) => {
+    db.execute(update_item_sql, [req.body.name, req.body.quantity, req.body.description, req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect(`/assignments/assignment_details/${req.params.id}`);
+        }
+    });
+})
+// start the server
+app.listen( port, () => {
+    console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
+} );
---
+++ db/db_init.js
@@ -0,0 +1,50 @@
+// (Re)Sets up the database, including a little bit of sample data
+const db = require("./db_connection");
+
+/**** Delete existing table, if any ****/
+const drop_stuff_table_sql = "DROP TABLE IF EXISTS `stuff`;"
+db.execute(drop_stuff_table_sql);
+
+/**** Create "stuff" table (again)  ****/
+const create_stuff_table_sql = `
+    CREATE TABLE stuff (
+        id INT NOT NULL AUTO_INCREMENT,
+        class VARCHAR(45) NOT NULL,
+        assignment VARCHAR(100) NOT NULL,
+        date VARCHAR(50) NOT NULL,
+        description VARCHAR(1000) NULL,
+        PRIMARY KEY (id)
+    );
+
+`
+db.execute(create_stuff_table_sql);
+
+/**** Create some sample items ****/
+const insert_stuff_table_sql = `
+    INSERT INTO stuff 
+        (class, assignment, date, description) 
+    VALUES 
+        (?, ?, ?, ?);
+`
+db.execute(insert_stuff_table_sql, ['Intro to WebApp', 'Prototype your app', 'January 6, 2023', 'Similar to the example "Inventory app" prototype, build your own prototype for a more specific inventory-keeping application of your own design. You may either build it largely from scratch (w/ Materialize of course) or start by copying my prototypes, and making edits to customize. Your prototype designs are not final, but they should look close to the way you intend the app to ultimately look']);
+
+db.execute(insert_stuff_table_sql, ['Chemistry', 'Unit 6 Test', 'February 27, 2023', 'Thermodynamics']);
+
+db.execute(insert_stuff_table_sql, ['AP CompSci', 'Test', 'February 10, 2023', 'Polymorphism']);
+
+db.execute(insert_stuff_table_sql, ['Health', 'Homework', 'February 12, 2023',null]);
+
+/**** Read the sample items inserted ****/
+const read_stuff_table_sql = "SELECT * FROM stuff";
+
+db.execute(read_stuff_table_sql, 
+    (error, results) => {
+        if (error) 
+            throw error;
+
+        console.log("Table 'stuff' initialized with:")
+        console.log(results);
+    }
+);
+
+db.end();
